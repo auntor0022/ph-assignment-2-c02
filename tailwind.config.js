21ef51cd2463f5a7e8a753b99a3bd962d4448aa1
Auntor <auntor0022@gmail.com>
2025-02-01 00:28:36 +0600

    added animation on banner section

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,17 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-    content: ["./src/**/*.{html,js}"],
-    theme: {
-      extend: {},
+  theme: {
+    extend: {
+      animation: {
+        bounceCustom: "bounceCustom 2s infinite",
+      },
+      keyframes: {
+        bounceCustom: {
+          "0%, 100%": { transform: "translateY(-5%)", animationTimingFunction: "cubic-bezier(0.8, 0, 1, 1)" },
+          "50%": { transform: "translateY(0)", animationTimingFunction: "cubic-bezier(0, 0, 0.2, 1)" },
+        },
+      },
     },
-    plugins: [],
-  }
+  },
+  plugins: [],
+};
